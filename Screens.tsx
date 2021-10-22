58365eb35c3c81932fc356824065140a2aae5a69
Hansraj <hrsirohiya@gmail.com>
2021-10-22 13:50:47 +0530

    first commit

--- Screens.tsx
+++ Screens.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState,useEffect} from 'react';
 import {RouteProp} from '@react-navigation/core';
 import {StackNavigationProp} from '@react-navigation/stack';
 import {
@@ -8,7 +8,9 @@ import {
     Button,
     TouchableOpacity,
     StyleSheet,
-    Alert
+    Alert,
+    FlatList,
+    SafeAreaView,ActivityIndicator
   } from 'react-native';
 import {RootStackParamList} from './App';
 import axios from 'axios';
@@ -27,90 +29,119 @@ type Props<T extends keyof RootStackParamList> = {
   navigation: ScreenNavigationProp<T>;
 };
 export const HomeScreen: React.FC<Props<'HomeScreen'>> = ({navigation}) => {
-    const [asteroidId, setAsteroidId] = useState("");
-   
+  const [loading, setLoading] = useState(true);
+  const [dataSource, setDataSource] = useState([]);
+  const [offset, setOffset] = useState(1);
 
-    const getDataUsingAsyncAwaitGetCall = async () => {
+  useEffect(() => getData(), []);
 
-        try {
-          const response = await axios.get(
-            'https://api.nasa.gov/neo/rest/v1/neo/'+asteroidId+'?api_key=j0JEsO6cmDRcbxefZ4EzfoOPZebG0kcYsPc2rJDS',
-          );
-          console.log('response---',JSON.stringify(response.data));
+  const getData = () => {
+    console.log('getData');
+    setLoading(true);
+    //Service to get the data from the server to render
+    fetch('https://hn.algolia.com/api/v1/search_by_date?tags=story&page='
+          + offset)
+      //Sending the currect offset with get request
+      .then((response) => response.json())
+      .then((responseJson) => {
+        //Successful response
+        setOffset(offset + 1);
+        //Increasing the offset for the next API call
+        setDataSource([...dataSource, ...responseJson.hits]);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
 
-             navigation.navigate('DetailsScreen', {
-            name:JSON.stringify(response.data.name),
-            nasaJplUri:JSON.stringify(response.data.nasa_jpl_url),
-            isPotentiallyHazardousAsteroid:JSON.stringify(response.data.is_potentially_hazardous_asteroid)
-            
+  const renderFooter = () => {
+    return (
+      //Footer View with Load More button
+      <View style={styles.footer}>
+        <TouchableOpacity
+          activeOpacity={0.9}
+          onPress={getData}
+          //On Click of button load more data
+          style={styles.loadMoreBtn}>
+          <Text style={styles.btnText}>Load More</Text>
+          {loading ? (
+            <ActivityIndicator
+              color="white"
+              style={{marginLeft: 8}} />
+          ) : null}
+        </TouchableOpacity>
+      </View>
+    );
+  };
+
+  const ItemView = ({item}) => {
+    return (
+      // Flat List Item
+      <TouchableOpacity
+      style={{padding:10,}}
+      onPress={() => 
+          navigation.navigate('DetailsScreen', {
+            name:JSON.stringify(item),
+            nasaJplUri:JSON.stringify(item.url),
+            isPotentiallyHazardousAsteroid:JSON.stringify(item.author)
+          
           })
+      }
+      >
+       <View style={styles.item}>
+        <Text>item: {item.title}
+        </Text>
+        <Text>URL: {item.url}
+        </Text>
+        <Text>author name: {item.author}
+        </Text>
+        </View>
 
 
-        } catch (error) {
-          // handle error
-          console.log('respoooooonse',JSON.stringify(error));
+      </TouchableOpacity>
+     
+      // <Text
+      //   style={styles.itemStyle}
+      //   onPress={() => getItem(item)}>
+      //   {item.id}
+      //   {'.'}
+      //   {item.title.toUpperCase()}
+      // </Text>
+    );
+  };
 
-          Alert.alert(JSON.stringify(error)) 
-        }
-      };
+  const ItemSeparatorView = () => {
+    return (
+      // Flat List Item Separator
+      <View
+        style={{
+          height: 0.5,
+          width: '100%',
+          backgroundColor: '#C8C8C8',
+        }}
+      />
+    );
+  };
 
-
-
-      const getDataUsingAsyncAwaitGetCall2 = async () => {
-
-        try {
-          const response = await axios.get(
-            'https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=j0JEsO6cmDRcbxefZ4EzfoOPZebG0kcYsPc2rJDS',
-          );
-          const randomNumber = Math.floor(Math.random() * response.data.near_earth_objects.length);
-          console.log('response---',JSON.stringify(response.data));
-            navigation.navigate('DetailsScreen', {
-            name:JSON.stringify(response.data.near_earth_objects[randomNumber].name),
-            nasaJplUri:JSON.stringify(response.data.near_earth_objects[randomNumber].nasa_jpl_url),
-            isPotentiallyHazardousAsteroid:JSON.stringify(response.data.near_earth_objects[randomNumber].is_potentially_hazardous_asteroid)
-
-            
-          })
-
-
-        } catch (error) {
-          // handle error
-          console.log('respoooooonse',JSON.stringify(error));
-          Alert.alert(JSON.stringify(error)) 
-         
-        }
-      };
+  const getItem = (item) => {
+    //Function for click on an item
+    alert('Id : ' + item.objectID + ' Title : ' + item.title);
+  };
 
   return (
-    <View style={styles.heading}>
-    <Text >Enter Asteroid Id </Text>
-    <View style={styles.form}>
-      <TextInput
-        style={styles.input}
-        placeholder="Enter Asteroid ID"
-        value={asteroidId}
-        keyboardType="numeric"
-        onChangeText={text => setAsteroidId(text)}
-      />
-
-       <Button
-        disabled={!asteroidId || asteroidId.length<7}
-        title="Submit"
-        onPress={getDataUsingAsyncAwaitGetCall}
-      />
-
-      <View style={styles.form}>
-      <Button
-        title="Random Asteroid'"
-        onPress={getDataUsingAsyncAwaitGetCall2}
-      />
+    <SafeAreaView style={{flex: 1}}>
+      <View style={styles.container}>
+        <FlatList
+          data={dataSource}
+          keyExtractor={(item, index) => index.toString()}
+          ItemSeparatorComponent={ItemSeparatorView}
+          enableEmptySections={true}
+          renderItem={ItemView}
+          ListFooterComponent={renderFooter}
+        />
       </View>
-
-     
-    </View>
-  </View>
-
-  
+    </SafeAreaView>
   );
 };
 
@@ -119,17 +150,40 @@ export const DetailsScreen: React.FC<Props<'DetailsScreen'>> = ({route}) => {
 
 return (
     <View style={{flex: 1, alignItems: 'flex-start', justifyContent: 'flex-start',padding:20}}>
-      <Text style={styles.buttonText} >Asteroid Details </Text>
-      <Text style={styles.titleText}>Name : <Text style={styles.valueText}>{name}</Text></Text>
-      <Text style={styles.titleText}>NasaJplUri : <Text style={styles.valueText}>{nasaJplUri}</Text></Text>
-      <Text style={styles.titleText}>IsPotentiallyHazardousAsteroid : <Text style={styles.valueText}>{isPotentiallyHazardousAsteroid}</Text></Text>
+     
+      <Text style={styles.titleText}>Item json : <Text style={styles.valueText}>{name}</Text></Text>
+      {/* <Text style={styles.titleText}>NasaJplUri : <Text style={styles.valueText}>{nasaJplUri}</Text></Text>
+      <Text style={styles.titleText}>IsPotentiallyHazardousAsteroid : <Text style={styles.valueText}>{isPotentiallyHazardousAsteroid}</Text></Text> */}
       {/* <Text>{otherParam}</Text> */}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-    heading: {
+  container: {
+    justifyContent: 'center',
+    flex: 1,
+  },
+  footer: {
+    padding: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  loadMoreBtn: {
+    padding: 10,
+    backgroundColor: '#800000',
+    borderRadius: 4,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  btnText: {
+    color: 'white',
+    fontSize: 15,
+    textAlign: 'center',
+  },  
+  heading: {
       marginTop:20,
       margin:20
       
@@ -158,9 +212,22 @@ const styles = StyleSheet.create({
       borderRadius: 5,
       alignItems: 'center',
     },
+    item: {
+      height: 100,
+      backgroundColor : '#e3e3e3',
+      marginVertical : 6,
+      justifyContent : 'center',
+      alignItems : 'center',
+      margin : 12,
+      padding:10,
+      borderRadius : 5,
+  },
     buttonText: {color: '#000',marginBottom:10,  fontSize: 20,fontWeight: "bold"},
    
     titleText: {color: '#000',marginBottom:10,  fontSize: 14,fontWeight: "bold"},
     valueText: {color: '#000',marginBottom:10,  fontSize: 13,fontWeight: "500"},
 
   })
+
+
+  
